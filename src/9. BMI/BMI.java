2e59a9534c610752ecafa06eebd80aeb6fc6da0c
dokutan <54861821+dokutan@users.noreply.github.com>
2020-10-09 12:29:27 +0200

    Fix indentation in BMI.java

--- src/9. BMI/BMI.java
+++ src/9. BMI/BMI.java
@@ -18,25 +18,25 @@ public class BMI {
 	}
 	
 	public static void main( String[] args ){
-	
-	// variables
-	double weight = 0.0, height = 0.0;
-	
-	// user input
-	if( args.length >= 2 ){
-		weight = Double.valueOf(args[0]);
-		height = Double.valueOf(args[1]);
-	} else {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Body weight in kg: ");
-		weight = sc.nextDouble();
-		System.out.print("Body height in m: ");
-		height = sc.nextDouble();
-	}
-	
-	// calculate and show BMI
-	DecimalFormat df = new DecimalFormat("0.0");
-	System.out.println("BMI: " + df.format( bmi(weight, height) ));
+		
+		// variables
+		double weight = 0.0, height = 0.0;
+		
+		// user input
+		if( args.length >= 2 ){
+			weight = Double.valueOf(args[0]);
+			height = Double.valueOf(args[1]);
+		} else {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Body weight in kg: ");
+			weight = sc.nextDouble();
+			System.out.print("Body height in m: ");
+			height = sc.nextDouble();
+		}
+		
+		// calculate and show BMI
+		DecimalFormat df = new DecimalFormat("0.0");
+		System.out.println("BMI: " + df.format( bmi(weight, height) ));
 	
 	}
 	
